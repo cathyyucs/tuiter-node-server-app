a34cb36c0bc59a4c10b6e23044caf35851b4828e
Jiaqian Yu <yu.ji@northeastern.edu>
2022-11-29 23:12:32 -0800

    a9 mongo fall 2022

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose"
 import express from 'express'
 import cors from 'cors'
 import HelloController
@@ -10,6 +11,10 @@ import TuitsController
 const app = express()
 app.use(cors())
 app.use(express.json())
+const DEV_CONNECTION_STRING = 'mongodb://localhost:27017/tuiter'
+const REMOTE_CONNECTION_STRING = 'mongodb+srv://wlfjiesfj:N8sdoX5NML6LKRJN@cluster0.ccbzkzk.mongodb.net/tuiter?retryWrites=true&w=majority'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || DEV_CONNECTION_STRING
+mongoose.connect(CONNECTION_STRING)
 
 TuitsController(app)
 HelloController(app)
